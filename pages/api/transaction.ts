74d8b9234a5f38695be57fe43a2e4c702fc8e4db
Cesar Oliveira <cesar.oliveira@yalo.com.br>
2021-12-02 12:54:29 -0300

    Database

--- pages/api/transaction.ts
+++ pages/api/transaction.ts
@@ -1,4 +1,23 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import { Db, MongoClient } from 'mongodb';
+
+let cachedDb: Db | undefined;
+
+async function connectToDatabase(uri: string) {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  const dataBaseClient = await MongoClient.connect(uri, {});
+
+  const dataBaseName = 'sensedata';
+
+  const dataBase = dataBaseClient.db(dataBaseName);
+
+  cachedDb = dataBase;
+
+  return dataBase;
+}
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (request: VercelRequest, response: VercelResponse) => {
@@ -11,10 +30,16 @@ export default async (request: VercelRequest, response: VercelResponse) => {
       });
       break;
     case 'POST':
-      const body = await request.body;
-      response.status(200).json({
-        body,
-      });
+      const body = request.body;
+
+      const dataBase = await connectToDatabase(process.env.MONGODB_URI || '');
+
+      const collection = dataBase.collection('transactions');
+
+      const result = await collection.insertOne(body);
+
+      response.status(200).json({ result });
+
       break;
     case 'PUT':
       response.status(200).json({
